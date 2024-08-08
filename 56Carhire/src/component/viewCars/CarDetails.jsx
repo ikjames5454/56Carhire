f7228be29f828ce1174b81c15968879986a32659
favouritesam <favsamlol@gmail.com>
2024-08-08 20:31:44 +0100

    still on implementation

---
+++ 56Carhire/src/component/viewCars/CarDetails.jsx
@@ -0,0 +1,67 @@
+import style from '../viewCars/carDetails.module.css'
+import {Button} from "@mui/material";
+export default function CarDetails() {
+    const carDetails = [
+        {
+            id: 1,
+            status: "Hired",
+            title: 'Mazda MX-5 Miata',
+            sportCar: 'Sport car',
+            automatic:"Automatic",
+            seats:"3 seats",
+            content: 'The Mazda MX-5 Miata is a classic, affordable sports car known for its lightweight, agile handling, ' +
+                'and engaging driving experience. With its sleek roadster design, it typically features a two-seater layout and' +
+                ' a convertible top. The Miata offers precise steering and a responsive, ' +
+                'high-revving engine, prioritizing driving fun over sheer power. Its driver-focused interior and overall value make it a favorite among driving enthusiasts.',
+            price: '₦30,000/hr'
+        },
+    ];
+
+    return (
+        <div className={'p-10'}>
+            {carDetails.map(car => (
+                <div key={car.id}>
+                    <span
+                        className={`${style.status} ${car.status === 'Hired' ? style.hired : style.available}`}
+                    >
+                        {car.status}
+                    </span>
+                    <div className={style.title}>
+                        <span> {car.title}</span>
+                    </div>
+                    <div className='flex flex-row space-x-4'>
+                        <span className={style.categories}>{car.sportCar}</span>
+                        <span className={style.categories}>{car.automatic}</span>
+                        <span className={style.categories}>{car.seats}</span>
+
+                    </div>
+                    <div className={style.content}>{car.content}</div>
+                    <div className={`${style.price} mt-7`}>{car.price}</div>
+                </div>
+            ))}
+            <div className='mt-12'>
+                <button
+                    id={'hireCar'}
+                    style={{
+                        color: '#FFFFFF',
+                        fontSize: '14px',
+                        fontWeight: 400,
+                        lineHeight: '24px',
+                        backgroundColor: '#007AFF',
+                        width: "488px",
+                        height: "41px",
+                        padding: "12px 10px",
+                        borderRadius: "4px",
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        textAlign: 'center',
+                        border: 'none',
+                        cursor: 'pointer'
+                }}>
+                Hire this car
+            </button>
+            </div>
+        </div>
+    );
+}
--- 56Carhire/src/route.jsx
+++ 56Carhire/src/route.jsx
@@ -1,5 +1,7 @@
 import Layout from "./Layout.jsx";
 import Hero from "./page/hero/Hero.jsx";
+import ViewAllCarDetails from "./component/viewCars/ViewAllCarDetails.jsx";
+import CarDetails from "./component/viewCars/CarDetails.jsx";
 
 export const Route = [
     {
@@ -9,6 +11,14 @@ export const Route = [
             {
                 path: '/',
                 element:  <Hero />, 
+            },
+            {
+                path: '/viewCars',
+                element:  <ViewAllCarDetails />,
+            },
+            {
+                path: '/carDetails',
+                element:  <CarDetails />,
             }
         ]
 
